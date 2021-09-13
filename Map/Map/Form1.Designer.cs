66d1b64d5c64baa888ce7f1dc49795972ca1c55b
Anahabi123 <79799497+Anahabi123@users.noreply.github.com>
2021-09-13 15:20:33 +0700

    addnewFeatured

--- Map/Map/Form1.Designer.cs
+++ Map/Map/Form1.Designer.cs
@@ -37,13 +37,13 @@ namespace Map
             this.Cambodia = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.addCountry = new System.Windows.Forms.PictureBox();
             this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.US)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.China)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Cambodia)).BeginInit();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.addCountry)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -102,20 +102,24 @@ namespace Map
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Gray;
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.addCountry);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, 887);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1389, 44);
             this.panel1.TabIndex = 2;
             // 
-            // pictureBox1
+            // addCountry
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(1341, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(45, 45);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.addCountry.ImageLocation = "https://png.pngtree.com/png-clipart/20190614/original/pngtree-add-vector-icon-png" +
+    "-image_3791307.jpg";
+            this.addCountry.Location = new System.Drawing.Point(1344, -1);
+            this.addCountry.Name = "addCountry";
+            this.addCountry.Size = new System.Drawing.Size(45, 45);
+            this.addCountry.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.addCountry.TabIndex = 2;
+            this.addCountry.TabStop = false;
+            this.addCountry.Click += new System.EventHandler(this.addCountry_Click);
             // 
             // guna2PictureBox1
             // 
@@ -152,7 +156,7 @@ namespace Map
             ((System.ComponentModel.ISupportInitialize)(this.Cambodia)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.addCountry)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -166,7 +170,7 @@ namespace Map
         private System.Windows.Forms.PictureBox Cambodia;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox addCountry;
         private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
     }
 }
---
+++ Map/Map/newCountry.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Map.Map
+{
+    partial class newCountry
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.description = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.countryName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pcb = new System.Windows.Forms.PictureBox();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(243, 77);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(224, 139);
+            this.description.TabIndex = 0;
+            this.description.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Description";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Country Name";
+            // 
+            // countryName
+            // 
+            this.countryName.Location = new System.Drawing.Point(243, 43);
+            this.countryName.Name = "countryName";
+            this.countryName.Size = new System.Drawing.Size(224, 22);
+            this.countryName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Picture";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(16, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "file location";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pcb
+            // 
+            this.pcb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pcb.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pcb.ImageLocation = "https://www.searchpng.com/wp-content/uploads/2019/02/User-Icon-PNG.png";
+            this.pcb.Location = new System.Drawing.Point(19, 43);
+            this.pcb.Name = "pcb";
+            this.pcb.Size = new System.Drawing.Size(100, 100);
+            this.pcb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcb.TabIndex = 2;
+            this.pcb.TabStop = false;
+            this.pcb.DragDrop += new System.Windows.Forms.DragEventHandler(this.pcb_DragDrop);
+            this.pcb.DragEnter += new System.Windows.Forms.DragEventHandler(this.pcb_DragEnter);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.BorderRadius = 4;
+            this.guna2Button3.CheckedState.Parent = this.guna2Button3;
+            this.guna2Button3.CustomImages.Parent = this.guna2Button3;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.DisabledState.Parent = this.guna2Button3;
+            this.guna2Button3.FillColor = System.Drawing.Color.Silver;
+            this.guna2Button3.Font = new System.Drawing.Font("Coolvetica Rg", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.guna2Button3.HoverState.Parent = this.guna2Button3;
+            this.guna2Button3.Location = new System.Drawing.Point(417, 0);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.ShadowDecoration.Parent = this.guna2Button3;
+            this.guna2Button3.Size = new System.Drawing.Size(53, 25);
+            this.guna2Button3.TabIndex = 5;
+            this.guna2Button3.Text = "×";
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button3_Click);
+            // 
+            // newCountry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.countryName);
+            this.Controls.Add(this.pcb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.description);
+            this.Name = "newCountry";
+            this.Size = new System.Drawing.Size(470, 297);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox description;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pcb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox countryName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+    }
+}
